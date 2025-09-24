5636166a167ec2f9e301ab218faffd5dbb075c6c
Denys G. Santos <>
2025-09-24 01:31:25 -0400

    first commit

---
+++ service-invocation/services/service-b/app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Request
+from prometheus_fastapi_instrumentator import Instrumentator
+
+app = FastAPI()
+
+Instrumentator().instrument(app).expose(app, endpoint="/metrics")
+
+@app.get("/health")
+def health():
+    return {"ok": True, "service": "payments"}
+
+@app.post("/charge")
+async def charge(req: Request):
+    data = await req.json()
+    # aqui você processaria o pagamento de fato
+    return {"status": "charged", "data": data}
